08fabf59c9ed7427d989d2e50b14b5bb6fc154ed
Ermakov Vladimir <goodstory152004@gmail.com>
2025-03-25 18:43:47 +0700

    laba2

---
+++ laba2/src/components/container.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Container({ children }) {
+  return (
+    <div style={{ border: "2px solid black", padding: "10px", margin: "10px" }}>
+
+      {children}
+    </div>
+  );
+}
+
+export default Container;
